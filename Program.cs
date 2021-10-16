eac646d2cb928563d18cf603c8cd5d154b8811ab
Vedang Singh <vedang012@gamil.com>
2021-10-16 18:52:24 +0530

    migrated to .net 5

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ namespace ToDoList
         {
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here");
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
             builder.Services.AddSyncfusionBlazor();
             builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
     }
